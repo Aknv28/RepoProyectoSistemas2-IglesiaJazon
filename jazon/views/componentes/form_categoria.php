aa73504f0295d95eddc44ea0d28798e6b9fb7a67
Aknv28 <nva2028246@est.univalle.edu>
2025-05-29 13:03:15 -0400

    Delete jazon directory

--- jazon/views/componentes/form_categoria.php
+++
@@ -1,5 +0,0 @@
-<div class="mb-3">
-    <label for="nombre" class="form-label">Nombre de la Categoria</label>
-    <input type="text" name="nombre" id="nombre" class="form-control" required>
-    <small id="error_nombre" class="text-danger"></small>
-</div>
--- jazon/views/componentes/form_zona.php
+++
@@ -1,5 +0,0 @@
-<div class="mb-3">
-    <label for="nombre_zona" class="form-label">Nombre de la Zona</label>
-    <input type="text" name="nombre_zona" id="nombre_zona" class="form-control" required>
-    <small id="error_nombre" class="text-danger"></small>
-</div>
